4d22adf4ca30a7c62f3fe8683ac06bbfb4a8396f
alex4401 <rylatgl@gmail.com>
2022-05-02 11:54:52 +0200

    feat: expose configs for used groups via mw.config

--- includes/DataMapEmbedRenderer.php
+++ includes/DataMapEmbedRenderer.php
@@ -22,13 +22,55 @@ class DataMapEmbedRenderer {
     }
 
     public function getJsConfigVariables(): array {
+        [$usedGroups, $usedLayers] = $this->getUsedMarkerTypes();
         return [
             'pageName' => $this->title->getPrefixedText(),
             'version' => $this->title->getLatestRevID(),
             'image' => $this->getImageUrl(),
             'imageBounds' => [ $this->image->getWidth(), $this->image->getHeight() ],
             'coordinateBounds' => $this->data->coordinateBounds,
+            'groups' => $this->getMarkerGroupsConfigsFor($usedGroups),
+            //'layers' => $this->array_map_kv('getMarkerLayerConfig', $usedLayers),
         ];
+    }
+
+    private function array_map_kv(string $callback, array $array) {
+        return array_reduce($array, function (array $results, string $item) {
+            $results[$item] = $this->$callback($item);
+            return $results;
+        }, array());
+    }
+
+    public function getUsedMarkerTypes(): array {
+        $groups = array();
+        $specifiers = array();
+        foreach (array_keys(get_object_vars($this->data->markers)) as &$name) {
+            $parts = explode(' ', $name);
+            $groups[] = array_shift($parts);
+            $specifiers += $parts;
+        }
+        return [array_unique($groups), array_unique($specifiers)];
+    }
+
+    public function getMarkerGroupsConfigsFor($names): array {
+        $results = array();
+        foreach ($names as &$name) {
+            $results[$name] = $this->getMarkerGroupConfig($name);
+        }
+        return $results;
+    }
+
+    public function getMarkerGroupConfig(string $name): array {
+        $info = $this->data->groups->$name;
+        return array(
+            'name' => $info->name,
+        );
+    }
+
+    public function getMarkerLayerConfig(string $name): array {
+        //$info = $this->data->groups->$name;
+        return array(
+        );
     }
 
     public function getModules(): array {
