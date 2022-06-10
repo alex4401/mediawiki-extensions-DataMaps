611d1d18e18d022fe0daf33bc0a2cffb20c5e573
alex4401 <rylatgl@gmail.com>
2022-06-10 21:26:23 +0200

    fix: parser function not reading named parameters right

--- includes/DataMapsHooks.php
+++ includes/DataMapsHooks.php
@@ -7,7 +7,7 @@ class DataMapsHooks {
     public static function onParserFirstCallInit( Parser $parser ) {
         $parser->setFunctionHook(
             'pf-embed-data-map',[ Rendering\ParserFunction_EmbedDataMap::class, 'run' ],
-            SFH_NO_HASH | SFH_OBJECT_ARGS
+            SFH_NO_HASH
         );
     }
 }
