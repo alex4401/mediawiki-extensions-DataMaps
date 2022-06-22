f6c7f8870331bfdf7b3cde42d290c82b6a4f1180
alex4401 <rylatgl@gmail.com>
2022-06-22 07:01:17 +0200

    fix: data download loses context to the last map on page
    Even though the `map` variable should not be reused between iterations, it happens. Probably some weird browser optimisation that fails?

--- resources/loader/main.js
+++ resources/loader/main.js
@@ -2,6 +2,25 @@ var DataMap = require( './map.js' ),
     initialisedMaps = [],
     api = new mw.Api();
 
+
+function finishMapInitialisation( map ) {
+    // Request markers from the API
+    var query = {
+        action: 'queryDataMap',
+        title: map.config.pageName,
+        revid: map.config.version
+    };
+    if ( map.dataSetFilters ) {
+        query.filter = map.dataSetFilters.join( '|' );
+    }
+    api.get( query ).then( function ( data ) {
+        map.waitForLeaflet( map.instantiateMarkers.bind( map, data.query.markers ) );
+    } ).then( function () {
+        map.$root.find( '.datamap-status' ).remove();
+    } );
+}
+
+
 // Begin initialisation once the document is loaded
 mw.hook( 'wikipage.content' ).add( function ( $content ) {
     var mapConfigs = mw.config.get( 'dataMaps' );
@@ -15,20 +34,8 @@ mw.hook( 'wikipage.content' ).add( function ( $content ) {
         mw.hook( 'ext.ark.datamaps.beforeInitialisation.' + id ).fire( config );
         var map = new DataMap( id, $content.find( '.datamap-container#datamap-' + id ), config );
 
-        // Request markers from the API
-        var query = {
-            action: 'queryDataMap',
-            title: config.pageName,
-            revid: config.version
-        };
-        if ( map.dataSetFilters ) {
-            query.filter = map.dataSetFilters.join( '|' );
-        }
-        api.get( query ).then( function ( data ) {
-            map.waitForLeaflet( map.instantiateMarkers.bind( map, data.query.markers ) );
-        } ).then( function () {
-            map.$root.find( '.datamap-status' ).remove();
-        } );
+        // Hand off to another function. Lower risk of losing control over the reference.
+        finishMapInitialisation( map );
 
         initialisedMaps.push( map );
         mw.hook( 'ext.ark.datamaps.afterInitialisation.' + id ).fire( map );
