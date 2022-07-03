eec637d68e35f3f617ee804f7e4943c2b3137a3e
alex4401 <rylatgl@gmail.com>
2022-07-03 12:52:48 +0200

    feat: display warnings when replaced fields are used, and allow saving

--- includes/Data/DataMapMarkerSpec.php
+++ includes/Data/DataMapMarkerSpec.php
@@ -47,11 +47,16 @@ class DataMapMarkerSpec extends DataModel {
 
     public function validate( Status $status ) {
         $this->requireField( $status, 'lat', DataModel::TYPE_NUMBER );
-        $this->requireField( $status, 'lon', DataModel::TYPE_NUMBER );
+        if ( !isset( $this->raw->long ) ) {
+            $this->requireField( $status, 'lon', DataModel::TYPE_NUMBER );
+        }
+        $this->allowReplacedField( $status, 'long', DataModel::TYPE_NUMBER, 'lon', 'v0.6.0', 'v0.9.0' );
         $this->expectField( $status, 'label', DataModel::TYPE_STRING );
         $this->expectField( $status, 'description', DataModel::TYPE_STRING );
         $this->expectField( $status, 'isWikitext', DataModel::TYPE_BOOL );
+        $this->allowReplacedField( $status, 'isDescriptionWikitext', DataModel::TYPE_BOOL, 'isWikitext', 'v0.7.0', 'v0.9.0' );
         $this->expectField( $status, 'article', DataModel::TYPE_STRING );
+        $this->allowReplacedField( $status, 'relatedArticle', DataModel::TYPE_STRING, 'article', 'v0.7.0', 'v0.9.0' );
         $this->expectField( $status, 'popupImage', DataModel::TYPE_STRING );
         $this->disallowOtherFields( $status );
 
