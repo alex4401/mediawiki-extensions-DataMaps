d5e1f6d301595bddf05e90bac5d7df8765832be8
alex4401 <rylatgl@gmail.com>
2022-08-31 17:56:25 +0200

    fix: inlineLoader uses deprecated subscribeHook function

--- resources/inlineLoader/main.js
+++ resources/inlineLoader/main.js
@@ -1,4 +1,4 @@
-mw.subscribeDataMapsHook( 'afterInitialisation', ( map ) => map.waitForLeaflet( () => {
+mw.dataMaps.subscribeHook( 'afterInitialisation', ( map ) => map.waitForLeaflet( () => {
     const dataNode = document.querySelector( `script#datamap-inline-data-${map.id}[type="application/json+datamap"]` );
     if ( dataNode ) {
         const data = JSON.parse( dataNode.innerText );
