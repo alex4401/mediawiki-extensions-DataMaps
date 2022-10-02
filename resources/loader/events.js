9601e44004e27ccef8c10eb70bbcea88f059b24e
alex4401 <rylatgl@gmail.com>
2022-10-02 15:15:13 +0200

    refactor!: rewrite EventEmitter onto ES6 classes

--- resources/loader/events.js
+++ resources/loader/events.js
@@ -1,53 +1,52 @@
-function EventEmitter() {
-    this._handlers = {};
-}
-
-
-EventEmitter.prototype.on = function ( event, callback, context ) {
-    if ( !this._handlers[event] ) {
-        this._handlers[event] = [];
+module.exports = class EventEmitter {
+    constructor() {
+        this._handlers = {};
     }
 
-    this._handlers[event].push( {
-        context: context || null,
-        method: callback
-    } );
-};
 
+    on( event, callback, context ) {
+        if ( !this._handlers[event] ) {
+            this._handlers[event] = [];
+        }
 
-EventEmitter.prototype.off = function ( event, callback ) {
-    if ( !callback ) {
-        delete this._handlers[event];
-        return;
+        this._handlers[event].push( {
+            context: context || null,
+            method: callback
+        } );
     }
 
-    if ( this._handlers[event] ) {
-        this._handlers[event] = this._handlers[event].filter( x => x.method != callback );
 
-        if ( this._handlers[event].length == 0 ) {
+    off( event, callback ) {
+        if ( !callback ) {
             delete this._handlers[event];
+            return;
+        }
+
+        if ( this._handlers[event] ) {
+            this._handlers[event] = this._handlers[event].filter( x => x.method != callback );
+
+            if ( this._handlers[event].length == 0 ) {
+                delete this._handlers[event];
+            }
         }
     }
-};
 
 
-EventEmitter.prototype.fire = function ( event ) {
-    if ( !this._handlers[event] ) {
-        return;
-    }
+    fire( event ) {
+        if ( !this._handlers[event] ) {
+            return;
+        }
 
-    const args = Object.values( arguments ).slice( 1 );
-    for ( const handler of this._handlers[event] ) {
-        try {
-            handler.method.apply( handler.context, args );
-        } catch ( error ) {
-            // If a listener throws an exception, do not disrupt the emitter's routine, and rethrow the exception later.
-            setTimeout( ( function ( error ) {
-                throw error;
-            } ).bind( null, error ) );
+        const args = Object.values( arguments ).slice( 1 );
+        for ( const handler of this._handlers[event] ) {
+            try {
+                handler.method.apply( handler.context, args );
+            } catch ( error ) {
+                // If a listener throws an exception, do not disrupt the emitter's routine, and rethrow the exception later
+                setTimeout( ( function ( error ) {
+                    throw error;
+                } ).bind( null, error ) );
+            }
         }
     }
-};
-
-
-module.exports = EventEmitter;
+}
