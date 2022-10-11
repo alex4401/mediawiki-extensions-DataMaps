ac6c185f7d85d15f6f29ea891f2339521e7b4b20
alex4401 <rylatgl@gmail.com>
2022-10-11 23:00:00 +0200

    refactor: yield metadata when rendering with no HTML

--- includes/Content/DataMapContent.php
+++ includes/Content/DataMapContent.php
@@ -117,12 +117,8 @@ class DataMapContent extends DataMapContentBase {
 	protected function fillParserOutput( Title $title, $revId, ParserOptions $options, $generateHtml, ParserOutput &$output ) {
 		$output = parent::fillParserOutput( $title, $revId, $options, $generateHtml, $output );
 
-		if ( !$generateHtml ) {
-			return $output;
-		}
-
 		if ( !$this->isMixin() ) {
-			if ( $options->getIsPreview() ) {
+			if ( $options->getIsPreview() && $generateHtml ) {
 				// If previewing an edit, run validation and end early on failure
 				$status = new Status();
 				$this->validateBeforeSave( $status );
@@ -139,7 +135,9 @@ class DataMapContent extends DataMapContentBase {
 			$embed = $this->getEmbedRenderer( $title, $parser, $options->getIsPreview() );
 			$embed->prepareOutput( $output );
 
-			$output->setText( $output->getRawText() . $embed->getHtml( new DataMapRenderOptions() ) );
+			if ( $generateHtml ) {
+				$output->setText( $output->getRawText() . $embed->getHtml( new DataMapRenderOptions() ) );
+			}
 		}
 
 		return $output;
