a435f684eb7f2d094491b3f6cc5bce86fa7e2273
alex4401 <rylatgl@gmail.com>
2022-10-14 18:10:45 +0200

    refactor!: rewrite MapStorage onto ES6 classes

--- resources/loader/storage.js
+++ resources/loader/storage.js
@@ -1,103 +1,105 @@
-function MapStorage( map, storageId ) {
-    this.map = map;
-    this.id = storageId || this.map.id;
-    this.hasSchemaVersion = false;
-    this.migrate();
-    this.dismissed = this.getArray( 'dismissed' );
+class MapStorage {
+    constructor( map, storageId ) {
+        this.map = map;
+        this.id = storageId || this.map.id;
+        this.hasSchemaVersion = false;
+        this.migrate();
+        this.dismissed = this.getArray( 'dismissed' );
+    }
+
+
+    get( name ) {
+        return localStorage.getItem( `ext.ark.datamaps.${this.id}:${name}` );
+    }
+
+
+    set( name, data ) {
+        this._initialiseVersioning();
+        localStorage.setItem( `ext.ark.datamaps.${this.id}:${name}`, data );
+    }
+
+
+    _initialiseVersioning() {
+        if ( !this.hasSchemaVersion ) {
+            this.hasSchemaVersion = true;
+            this.set( 'schemaVersion', MapStorage.LATEST_VERSION );
+        }
+    }
+
+
+    getArray( name ) {
+        return JSON.parse( this.get( name ) || '[]' );
+    }
+
+
+    setObject( name, data ) {
+        this.set( name, JSON.stringify( data ) );
+    }
+
+
+    migrate() {
+        // Check if any saved properties exist, and if not, abort
+        if ( !this.get( 'dismissed' ) ) {
+            return;
+        }
+
+        const schemaVersion = this.get( 'schemaVersion' ) || -1;
+        this.hasSchemaVersion = true;
+        let shouldUpdateVersion = false;
+
+        switch ( schemaVersion ) {
+            case -1:
+                shouldUpdateVersion = true;
+                // Drop the #surface layer from memorised dismissed markers
+                this.setObject( 'dismissed', this.getArray( 'dismissed' ).map( x => x.replace( ' #surface', '' ) ) );
+            case '20220713':
+                shouldUpdateVersion = true;
+                // Parse dismissed marker IDs and use fixed precision on coordinates
+                this.setObject( 'dismissed', this.getArray( 'dismissed' ).map( x => {
+                    const a = x.split( '@' );
+                    const b = a[1].split( ':' );
+                    const lat = parseFloat( b[0] );
+                    const lon = parseFloat( b[1] );
+                    return ( lat == NaN || lon == NaN ) ? x : ( a[0] + '@' + lat.toFixed( 3 ) + ':' + lon.toFixed( 3 ) );
+                } ) );
+                break;
+            case '20220803':
+                shouldUpdateVersion = true;
+                // Add marker namespace to every dismissed ID
+                this.setObject( 'dismissed', this.getArray( 'dismissed' ).map( x => 'M:' + x ) );
+        }
+
+        if ( shouldUpdateVersion ) {
+            this.set( 'schemaVersion', MapStorage.LATEST_VERSION );
+        }
+    }
+
+
+    isDismissed( uid, isGroup ) {
+        if ( this.dismissed.length === 0 ) {
+            return false;
+        }
+        return this.dismissed.indexOf( ( isGroup ? 'G:' : 'M:' ) + uid ) >= 0;
+    }
+
+
+    toggleDismissal( uid, isGroup ) {
+        let out;
+        const uidPrefixed = ( isGroup ? 'G:' : 'M:' ) + uid;
+        if ( this.isDismissed( uid, isGroup ) ) {
+            this.dismissed = this.dismissed.filter( x => x != uidPrefixed );
+            out = false;
+        } else {
+            this.dismissed.push( uidPrefixed );
+            out = true;
+        }
+        this.setObject( 'dismissed', this.dismissed );
+        return out;
+    }
 }
 
-
-MapStorage.prototype.LATEST_VERSION = 20220929;
-
-
-MapStorage.prototype.get = function ( name ) {
-    return localStorage.getItem( `ext.ark.datamaps.${this.id}:${name}` );
-};
-
-
-MapStorage.prototype.set = function ( name, data ) {
-    this.initialiseFirstWrite();
-    localStorage.setItem( `ext.ark.datamaps.${this.id}:${name}`, data );
-};
-
-
-MapStorage.prototype.initialiseFirstWrite = function () {
-    if ( !this.hasSchemaVersion ) {
-        this.hasSchemaVersion = true;
-        this.set( 'schemaVersion', MapStorage.prototype.LATEST_VERSION );
-    }
-};
-
     
-MapStorage.prototype.getArray = function ( name ) {
-    return JSON.parse( this.get( name ) || '[]' );
-};
-
-    
-MapStorage.prototype.setObject = function ( name, data ) {
-    this.set( name, JSON.stringify( data ) );
-};
+MapStorage.LATEST_VERSION = 20220929;
 
 
-MapStorage.prototype.migrate = function () {
-    // Check if any saved properties exist, and if not, abort
-    if ( !this.get( 'dismissed' ) ) {
-        return;
-    }
-
-    const schemaVersion = this.get( 'schemaVersion' ) || -1;
-    this.hasSchemaVersion = true;
-    let shouldUpdateVersion = false;
-
-    switch ( schemaVersion ) {
-        case -1:
-            shouldUpdateVersion = true;
-            // Drop the #surface layer from memorised dismissed markers
-            this.setObject( 'dismissed', this.getArray( 'dismissed' ).map( x => x.replace( ' #surface', '' ) ) );
-        case '20220713':
-            shouldUpdateVersion = true;
-            // Parse dismissed marker IDs and use fixed precision on coordinates
-            this.setObject( 'dismissed', this.getArray( 'dismissed' ).map( x => {
-                const a = x.split( '@' );
-                const b = a[1].split( ':' );
-                const lat = parseFloat( b[0] );
-                const lon = parseFloat( b[1] );
-                return ( lat == NaN || lon == NaN ) ? x : ( a[0] + '@' + lat.toFixed( 3 ) + ':' + lon.toFixed( 3 ) );
-            } ) );
-            break;
-        case '20220803':
-            shouldUpdateVersion = true;
-            // Add marker namespace to every dismissed ID
-            this.setObject( 'dismissed', this.getArray( 'dismissed' ).map( x => 'M:' + x ) );
-    }
-
-    if ( shouldUpdateVersion ) {
-        this.set( 'schemaVersion', MapStorage.prototype.LATEST_VERSION );
-    }
-};
-
-
-MapStorage.prototype.isDismissed = function ( uid, isGroup ) {
-    if ( this.dismissed.length === 0 ) {
-        return false;
-    }
-    return this.dismissed.indexOf( ( isGroup ? 'G:' : 'M:' ) + uid ) >= 0;
-};
-
-
-MapStorage.prototype.toggleDismissal = function ( uid, isGroup ) {
-    let out;
-    const uidPrefixed = ( isGroup ? 'G:' : 'M:' ) + uid;
-    if ( this.isDismissed( uid, isGroup ) ) {
-        this.dismissed = this.dismissed.filter( x => x != uidPrefixed );
-        out = false;
-    } else {
-        this.dismissed.push( uidPrefixed );
-        out = true;
-    }
-    this.setObject( 'dismissed', this.dismissed );
-    return out;
-};
-
-    
 module.exports = MapStorage;
