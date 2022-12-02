ab168bccf5b73d06b98d6a2f30d7729b3195a0fe
alex4401 <rylatgl@gmail.com>
2022-12-02 22:34:48 +0100

    docs: mark CircleMarker and IconMarker subclasses as deprecated

--- modules/leafletPkg/index.js
+++ modules/leafletPkg/index.js
@@ -1,6 +1,9 @@
 module.exports = require( '../vendor/leaflet/leaflet.js' );
 module.exports.Ark = {
+    /** @deprecated */
     CircleMarker: require( './circleMarker.js' ),
+    /** @deprecated */
     IconMarker: require( './iconMarker.js' ),
+
     Popup: require( './popup.js' )
 };
