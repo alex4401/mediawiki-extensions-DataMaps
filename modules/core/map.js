e5970a50036da62e7eda17c0aea77884094b191c
alex4401 <rylatgl@gmail.com>
2023-03-08 22:03:29 +0100

    fix: addControl(..., prepend: true) does not respect the control group

--- modules/core/map.js
+++ modules/core/map.js
@@ -943,7 +943,7 @@ class DataMap extends EventEmitter {
             anchorElement = this.resolveControlAnchor( anchor ),
             beforeInlineGroup = prepend && anchorElement.querySelector( ':scope > .ext-datamaps-control-group' );
         if ( beforeInlineGroup ) {
-            anchorElement.insertBefore( controlElement, beforeInlineGroup );
+            anchorElement.insertBefore( controlElement, beforeInlineGroup.nextSibling );
         } else {
             anchorElement[ prepend ? 'prepend' : 'appendChild' ]( controlElement );
         }
