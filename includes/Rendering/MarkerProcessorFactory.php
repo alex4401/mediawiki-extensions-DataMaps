cad16b18a5826b13eaed46d586f245a2593a96e0
alex4401 <rylatgl@gmail.com>
2023-09-24 00:46:16 +0200

    refactor: extend MarkerProcessorFactory to allow using existing parsers

--- includes/Rendering/MarkerProcessorFactory.php
+++ includes/Rendering/MarkerProcessorFactory.php
@@ -6,6 +6,7 @@ use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Extension\DataMaps\ConfigNames;
 use MediaWiki\Extension\DataMaps\Data\DataMapSpec;
 use MediaWiki\Extension\DataMaps\ExtensionConfig;
+use Parser;
 use ParserFactory;
 use ParserOptions;
 use Title;
@@ -21,6 +22,10 @@ final class MarkerProcessorFactory {
     /** @var ExtensionConfig */
     private ExtensionConfig $config;
 
+    /**
+     * @param ParserFactory $parserFactory
+     * @param ExtensionConfig $config
+     */
     public function __construct(
         ParserFactory $parserFactory,
         ExtensionConfig $config
@@ -29,16 +34,37 @@ final class MarkerProcessorFactory {
         $this->config = $config;
     }
 
-    public function createParserOptions(): ParserOptions {
+    /**
+     * Returns parser options for marker wikitext expansion.
+     *
+     * @param ParserOptions|null $originalOptions Options to use the current revision callback from.
+     * @return ParserOptions
+     */
+    public function createParserOptions( ?ParserOptions $originalOptions = null ): ParserOptions {
         $result = ParserOptions::newFromAnon();
         $result->setAllowSpecialInclusion( false );
         $result->setExpensiveParserFunctionLimit( 5 );
         $result->setInterwikiMagic( false );
         $result->setMaxIncludeSize( $this->config->getParserExpansionLimit() );
+
+        if ( $originalOptions ) {
+            $result->setCurrentRevisionRecordCallback( $originalOptions->getCurrentRevisionRecordCallback() );
+        }
+
         return $result;
     }
 
-    public function create(
+    /**
+     * Creates a processor instance using the provided wikitext parser instance.
+     *
+     * @param Parser $parser Wikitext parser to use.
+     * @param Title $title Page reference to the map.
+     * @param DataMapSpec $dataMap Map to process.
+     * @param array|null $filter Layers to render.
+     * @return MarkerProcessor
+     */
+    public function createUsingParser(
+        Parser $parser,
         Title $title,
         DataMapSpec $dataMap,
         ?array $filter
@@ -49,10 +75,31 @@ final class MarkerProcessorFactory {
         }
 
         return new MarkerProcessor(
-            $this->parserFactory->create(),
-            $this->createParserOptions(),
+            $parser,
+            $this->createParserOptions( $parser->getOptions() ),
             $this->config,
             $lru,
+            $title,
+            $dataMap,
+            $filter
+        );
+    }
+
+    /**
+     * Creates a processor instance with a parser instance acquired from the factory.
+     *
+     * @param Title $title Page reference to the map.
+     * @param DataMapSpec $dataMap Map to process.
+     * @param array|null $filter Layers to render.
+     * @return MarkerProcessor
+     */
+    public function create(
+        Title $title,
+        DataMapSpec $dataMap,
+        ?array $filter
+    ): MarkerProcessor {
+        return $this->createUsingParser(
+            $this->parserFactory->create(),
             $title,
             $dataMap,
             $filter
