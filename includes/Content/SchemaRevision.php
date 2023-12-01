92c93755d5c65a84db689e5314ae7355e8cb343a
alex4401 <rylatgl@gmail.com>
2023-12-01 12:12:21 +0100

    fix: v0.16 schema was renamed to `v16`

--- includes/Content/SchemaRevision.php
+++ includes/Content/SchemaRevision.php
@@ -2,6 +2,6 @@
 namespace MediaWiki\Extension\DataMaps\Content;
 
 final class SchemaRevision {
-    public const REV_16 = 'v0.16';
+    public const REV_16 = 'v16';
     public const REV_17 = 'v17';
 }
