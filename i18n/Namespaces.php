6846010d631421806b0384ca912be5144483bb4e
alex4401 <rylatgl@gmail.com>
2024-02-17 07:49:41 +0100

    feat: localisation support for the Map namespace
    Issue: #224

---
+++ i18n/Namespaces.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Internationalisation file for extension DataMaps.
+ */
+
+$namespaceNames = [];
+$namespaceAliases = [];
+
+$namespaceNames['en'] = [
+    2900 => 'Map',
+    2901 => 'Map_talk',
+];
