283bec03775b7bedfe3561ac9c4911286bcb5a0f
alex4401 <rylatgl@gmail.com>
2024-02-23 15:53:38 +0100

    refactor: lazy-loaded search controller and initialisation
    Issue: #232

--- modules/search/SearchController.js
+++ modules/search/SearchController.js
@@ -5,8 +5,8 @@ const
         Controls
     } = require( 'ext.datamaps.core' ),
     Leaflet = require( 'ext.datamaps.leaflet' ),
-    MarkerSearchIndex = require( './MarkerSearchIndex.js' ),
-    getNonNull = Util.getNonNull;
+    SearchIndex = require( './SearchIndex.js' ),
+    { createDomElement, getNonNull } = Util;
 
 
 /**
@@ -14,25 +14,47 @@ const
  * @property {HTMLElement} [element]
  */
 /**
- * @typedef {MarkerSearchIndex.Item & ItemExtensionProps} Item
+ * @typedef {SearchIndex.Item & ItemExtensionProps} Item
  */
 
 
-class MarkerSearch extends Controls.MapControl {
+class SearchController {
     /**
-     * @param {InstanceType<DataMap>} map Owning map.
-     * @param {MarkerSearchIndex} index
-     * @param {boolean} isLinked
+     * @param {InstanceType<typeof Controls.SearchHost>} control
+     * @param {OO.ui.TextInputWidget} inputBox
+     * @param {SearchIndex} index
      */
-    constructor( map, index, isLinked ) {
-        super( map, 'search' );
-
-        /** @type {MarkerSearchIndex} */
-        this.ownedIndex = index;
-        /** @type {MarkerSearchIndex?} */
-        this.displayIndex = null;
-        /** @type {boolean} */
-        this.isLinked = isLinked;
+    constructor( control, inputBox, index ) {
+        /**
+         * @private
+         * @type {InstanceType<typeof Controls.SearchHost>}
+         */
+        this._control = control;
+        /**
+         * @private
+         * @type {InstanceType<DataMap>}
+         */
+        this._map = control.map;
+        /**
+         * @private
+         * @type {OO.ui.TextInputWidget}
+         */
+        this._inputBox = inputBox;
+        /**
+         * @private
+         * @type {SearchIndex}
+         */
+        this._ownedIndex = index;
+        /**
+         * @private
+         * @type {SearchIndex?}
+         */
+        this._displayIndex = null;
+        /**
+         * @private
+         * @type {boolean}
+         */
+        this._isLinked = index instanceof SearchIndex.ChildIndex;
         /**
          * @private
          * @type {Item[]?}
@@ -48,51 +70,48 @@ class MarkerSearch extends Controls.MapControl {
          * @type {number?}
          */
         this._ignoreTimeout = null;
-
-        // UI construction
-
-        /**
-         * @private
-         * @type {OO.ui.TextInputWidget}
-         */
-        this._inputBox = new OO.ui.TextInputWidget( {
-            placeholder: mw.msg( 'datamap-control-search' ),
-            icon: 'search'
-        } );
-        this._inputBox.$element.appendTo( this.element );
         /**
          * @private
          * @type {HTMLElement}
          */
-        this._innerElement = Util.createDomElement( 'ul', {
+        this._listElement = createDomElement( 'ul', {
             classes: [ 'ext-datamaps-control-search-results' ],
             appendTo: this._inputBox.$element[ 0 ]
         } );
 
-        if ( this.isLinked ) {
-            this.linkedToggle = new OO.ui.ToggleButtonWidget( {
+        /**
+         * @private
+         * @type {OO.ui.ToggleButtonWidget?}
+         */
+        this._linkedToggle = null;
+        if ( this._isLinked ) {
+            this._linkedToggle = new OO.ui.ToggleButtonWidget( {
                 label: mw.msg( 'datamap-control-search-toggle-sharing' ),
                 value: true
             } );
-            this.linkedToggle.on( 'change', value => this._setDisplayIndex( value ? this.ownedIndex.parent : this.ownedIndex ) );
-            this.linkedToggle.$element.insertBefore( this._inputBox.$indicator );
+            this._linkedToggle.on( 'change', value => {
+                this._setDisplayIndex( value ? this._ownedIndex.parent : this._ownedIndex );
+            } );
+            this._linkedToggle.$element.insertBefore( this._inputBox.$indicator );
         }
 
         // Initialisation logic
 
         // Build the index from markers that have been loaded so far
-        this._setDisplayIndex( this.isLinked ? /** @type {MarkerSearchIndex.ChildIndex} */ ( this.ownedIndex ).parent
-            : this.ownedIndex );
+        this._setDisplayIndex(
+            this._isLinked
+                ? /** @type {InstanceType<SearchIndex.ChildIndex>} */ ( this._ownedIndex ).parent
+                : this._ownedIndex
+        );
         this.addExistingMarkersToOwnIndex();
         // Set up event handlers
-        Util.preventMapInterference( this.element );
         this._inputBox.$input.on( 'mousedown', () => this.toggle( true ) );
         this._inputBox.$input[ 0 ].addEventListener( 'keydown', event => this._handleInputKeyDownEvent( event ) );
         this._inputBox.on( 'change', () => this._handleInputChange() );
-        this._innerElement.addEventListener( 'click', () => this._inputBox.$input[ 0 ].focus() );
-        Util.getNonNull( this.map.viewport ).getLeafletMap().on( 'click', () => this.toggle( false ), this );
-        this.map.on( 'markerReady', this.addMarker, this );
-        this.map.on( 'chunkStreamingDone', this._onChunkStreamed, this );
+        this._listElement.addEventListener( 'click', () => this._inputBox.$input[ 0 ].focus() );
+        Util.getNonNull( this._map.viewport ).getLeafletMap().on( 'click', () => this.toggle( false ), this );
+        this._map.on( 'markerReady', this.addMarker, this );
+        this._map.on( 'chunkStreamingDone', this._onChunkStreamed, this );
     }
 
 
@@ -150,10 +169,10 @@ class MarkerSearch extends Controls.MapControl {
      * Inserts markers from the map to the index owned by this control.
      */
     addExistingMarkersToOwnIndex() {
-        for ( const leafletMarker of this.map.layerManager.markers ) {
-            this.ownedIndex.add( this.map, leafletMarker );
+        for ( const leafletMarker of this._map.layerManager.markers ) {
+            this._ownedIndex.add( this._map, leafletMarker );
         }
-        this.ownedIndex.commit();
+        this._ownedIndex.commit();
     }
 
 
@@ -163,7 +182,7 @@ class MarkerSearch extends Controls.MapControl {
      * @param {LeafletModule.AnyMarker} leafletMarker
      */
     addMarker( leafletMarker ) {
-        this.ownedIndex.add( this.map, leafletMarker );
+        this._ownedIndex.add( this._map, leafletMarker );
     }
 
 
@@ -171,22 +190,22 @@ class MarkerSearch extends Controls.MapControl {
      * @private
      */
     _onChunkStreamed() {
-        this.ownedIndex.commit();
+        this._ownedIndex.commit();
     }
 
 
     /**
-     * @param {MarkerSearchIndex} index
+     * @param {SearchIndex} index
      */
     _setDisplayIndex( index ) {
-        if ( this.displayIndex ) {
-            this.displayIndex.off( 'commit', this.refreshItems, this );
+        if ( this._displayIndex ) {
+            this._displayIndex.off( 'commit', this.refreshItems, this );
         }
 
-        this.displayIndex = index;
+        this._displayIndex = index;
         this.refreshItems();
 
-        this.displayIndex.on( 'commit', this.refreshItems, this );
+        getNonNull( this._displayIndex ).on( 'commit', this.refreshItems, this );
     }
 
 
@@ -198,7 +217,7 @@ class MarkerSearch extends Controls.MapControl {
         this._inputBox.setValue( '', true );
         this.toggle( false );
         item.map.openMarkerPopup( item.leafletMarker, true );
-        const tabber = /** @type {HTMLElement?} */ ( this.isLinked && item.map !== this.map
+        const tabber = /** @type {HTMLElement?} */ ( this._isLinked && item.map !== this._map
             ? getNonNull( Util.TabberNeue.getOwningTabber( item.map.rootElement ) ).querySelector( '#' + getNonNull(
                 Util.TabberNeue.getOwningPanel( item.map.rootElement ) ).getAttribute( 'aria-labelledby' ) )
             : null );
@@ -212,10 +231,10 @@ class MarkerSearch extends Controls.MapControl {
      * @private
      */
     _showNoItemsMessage() {
-        this._innerElement.textContent = '';
-        Util.createDomElement( 'li', {
+        this._listElement.textContent = '';
+        createDomElement( 'li', {
             text: mw.msg( 'datamap-control-search-no-results' ),
-            appendTo: this._innerElement
+            appendTo: this._listElement
         } );
     }
 
@@ -232,10 +251,10 @@ class MarkerSearch extends Controls.MapControl {
 
         if ( item === 'next' ) {
             this._highlighted = /** @type {HTMLElement?} */ ( this._highlighted && this._highlighted.nextSibling
-                || this._innerElement.firstChild );
+                || this._listElement.firstChild );
         } else if ( item === 'previous' ) {
             this._highlighted = /** @type {HTMLElement?} */ ( this._highlighted && this._highlighted.previousSibling
-                || this._innerElement.lastChild );
+                || this._listElement.lastChild );
         } else if ( typeof item !== 'string' ) {
             this._highlighted = item;
         }
@@ -271,8 +290,8 @@ class MarkerSearch extends Controls.MapControl {
         }
 
         // Clear and scroll to top
-        this._innerElement.textContent = '';
-        this._innerElement.scrollTop = 0;
+        this._listElement.textContent = '';
+        this._listElement.scrollTop = 0;
 
         if ( !this._cachedItems || this._cachedItems.length === 0 ) {
             this._showNoItemsMessage();
@@ -281,7 +300,7 @@ class MarkerSearch extends Controls.MapControl {
 
         for ( const item of this._cachedItems ) {
             if ( !item.element ) {
-                item.element = Util.createDomElement( 'li', {
+                item.element = createDomElement( 'li', {
                     html: item.label,
                     attributes: {
                         role: 'option',
@@ -308,7 +327,7 @@ class MarkerSearch extends Controls.MapControl {
                         iconElement.setAttribute( 'width', '16' );
                         iconElement.setAttribute( 'height', '16' );
                     } else {
-                        iconElement = Util.createDomElement( 'img', {
+                        iconElement = createDomElement( 'img', {
                             attributes: {
                                 width: 16,
                                 height: 16,
@@ -320,7 +339,7 @@ class MarkerSearch extends Controls.MapControl {
                 }
 
                 if ( badgeInfo ) {
-                    Util.createDomElement( 'span', {
+                    createDomElement( 'span', {
                         classes: [ 'ext-datamaps-search-badge' ],
                         attributes: {
                             'data-current': badgeInfo[ 1 ]
@@ -333,7 +352,7 @@ class MarkerSearch extends Controls.MapControl {
         }
 
         for ( const item of Util.getNonNull( this._cachedItems ) ) {
-            this._innerElement.appendChild( Util.getNonNull( item.element ) );
+            this._listElement.appendChild( Util.getNonNull( item.element ) );
         }
 
         if ( this._highlighted && this._highlighted.parentNode !== null ) {
@@ -351,7 +370,7 @@ class MarkerSearch extends Controls.MapControl {
      * @return {[ name: string, current: boolean ]?}
      */
     _getItemBadge( item ) {
-        if ( this.isLinked && item.map !== this.map ) {
+        if ( this._isLinked && item.map !== this._map ) {
             const title = getNonNull( Util.TabberNeue.getOwningPanel( item.map.rootElement ) ).getAttribute( 'data-title' );
             return title ? [ title, false ] : null;
         }
@@ -370,8 +389,8 @@ class MarkerSearch extends Controls.MapControl {
 
     refreshItems() {
         const query = this._inputBox.getValue();
-        if ( this.displayIndex ) {
-            this._cachedItems = query === '' ? this.displayIndex.items : this.displayIndex.query( query ).map( x => x.obj );
+        if ( this._displayIndex ) {
+            this._cachedItems = query === '' ? this._displayIndex.items : this._displayIndex.query( query ).map( x => x.obj );
         } else {
             this._cachedItems = null;
         }
@@ -380,7 +399,7 @@ class MarkerSearch extends Controls.MapControl {
 
 
     isOpen() {
-        return this.element.getAttribute( 'aria-expanded' ) === 'true';
+        return this._control.element.getAttribute( 'aria-expanded' ) === 'true';
     }
 
 
@@ -388,10 +407,10 @@ class MarkerSearch extends Controls.MapControl {
      * @param {boolean} value
      */
     toggle( value ) {
-        this.element.setAttribute( 'aria-expanded', value ? 'true' : 'false' );
+        this._control.element.setAttribute( 'aria-expanded', value ? 'true' : 'false' );
         this._renderItems();
     }
 }
 
 
-module.exports = MarkerSearch;
+module.exports = SearchController;
