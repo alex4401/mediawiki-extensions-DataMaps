94f95f969522a74f5ef8026f0eb55308b38d89ad
alex4401 <rylatgl@gmail.com>
2024-05-01 20:41:13 +0200

    schema: read REV as a float

--- schemas/defs.py
+++ schemas/defs.py
@@ -5,7 +5,7 @@ from typing import Annotated, Any, Literal, Optional
 
 
 PROPERTIES = dict(el.split('=', 2) for el in sys.argv if '=' in el)
-REV = int(PROPERTIES['REV'])
+REV = float(PROPERTIES['REV'])
 FRAGMENT = PROPERTIES['FRAGMENT'] != '0'
 OUTPUT = PROPERTIES['OUT']
 
