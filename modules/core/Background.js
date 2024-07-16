3dc43ea6a6e16f67551e5510d9a28ab114c5da88
alex4401 <rylatgl@gmail.com>
2024-07-16 05:32:20 +0200

    refactor: let Leaflet set the tile class, use getTileSize for canvas sizing
    Issue: #220

--- modules/core/Background.js
+++ modules/core/Background.js
@@ -153,11 +153,11 @@ class Background extends EventEmitter {
 
         const dataMapsTile = Leaflet.GridLayer.extend( {
             createTile( coords, doneCallback ) {
-                const tile = Leaflet.DomUtil.create( 'canvas', 'leaflet-tile' );
+                const tile = document.createElement( 'canvas' ),
+                    tileSize = this.getTileSize();
+                tile.width = tileSize.x;
+                tile.height = tileSize.y;
                 tile.setAttribute( 'src', getImageUrlByPosition( coords ) );
-
-                tile.width = size[ 0 ];
-                tile.height = size[ 1 ];
 
                 const ctx = tile.getContext( '2d' );
                 const imgSrc = getImageUrlByPosition( coords );
